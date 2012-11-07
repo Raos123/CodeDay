2cff7bc5b6e74dc0cf6feb9c7dba736c8a1ceef0
jroman <jroman@covermymeds.com>
2012-11-07 09:53:39 -0500

    Rest of initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeKeepingSystem::Application.config.secret_token = '2cf052bb7903e32fce0cff51a2345f2a05679264203d1c19225374302514fba18a2d5772650d5ea26aab232063a17f6b26a28aa2eeadb54e7eab3eba2c6a48de'
